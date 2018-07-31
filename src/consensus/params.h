a6c91fe84b978413c254d0d6a77d7de6f25978a8
heidi-dang <heidi.dang.dev@gmail.com>
2018-07-31 11:09:22 +0700

    Add NO_TIMEOUT and ALWAYS_ACTIVE constants for BIP 9 deployments

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -1,35 +1,49 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Copyright (c) 2017-2018 The Luxcore developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_CONSENSUS_PARAMS_H
 #define BITCOIN_CONSENSUS_PARAMS_H
 
-#include "uint256.h"
+#include <uint256.h>
+#include <limits>
+#include <map>
+#include <string>
 
 namespace Consensus {
 
-    enum DeploymentPos
-    {
-        DEPLOYMENT_TESTDUMMY,
-        DEPLOYMENT_CSV, // Deployment of BIP68, BIP112, and BIP113.
-        DEPLOYMENT_SEGWIT, // Deployment of BIP141 and BIP143
-        // NOTE: Also add new deployments to VersionBitsDeploymentInfo in versionbits.cpp
-        SMART_CONTRACTS_HARDFORK,
-        MAX_VERSION_BITS_DEPLOYMENTS
-    };
+enum DeploymentPos
+{
+    DEPLOYMENT_TESTDUMMY,
+    DEPLOYMENT_CSV, // Deployment of BIP68, BIP112, and BIP113.
+    DEPLOYMENT_SEGWIT, // Deployment of BIP141, BIP143, and BIP147.
+    // NOTE: Also add new deployments to VersionBitsDeploymentInfo in versionbits.cpp
+    SMART_CONTRACTS_HARDFORK,
+    MAX_VERSION_BITS_DEPLOYMENTS
+};
 
-    /**
-     * Struct for each individual consensus rule change using BIP9.
-     */
-    struct BIP9Deployment {
-        /** Bit position to select the particular bit in nVersion. */
-        int bit;
-        /** Start MedianTime for version bits miner confirmation. Can be a date in the past */
-        int64_t nStartTime;
-        /** Timeout/expiry MedianTime for the deployment attempt. */
-        int64_t nTimeout;
-    };
+/**
+ * Struct for each individual consensus rule change using BIP9.
+ */
+struct BIP9Deployment {
+    /** Bit position to select the particular bit in nVersion. */
+    int bit;
+    /** Start MedianTime for version bits miner confirmation. Can be a date in the past */
+    int64_t nStartTime;
+    /** Timeout/expiry MedianTime for the deployment attempt. */
+    int64_t nTimeout;
+
+    /** Constant for nTimeout very far in the future. */
+    static constexpr int64_t NO_TIMEOUT = std::numeric_limits<int64_t>::max();
+
+    /** Special value for nStartTime indicating that the deployment is always active.
+     *  This is useful for testing, as it means tests don't need to deal with the activation
+     *  process (which takes at least 3 BIP9 intervals). Only tests that specifically test the
+     *  behaviour during activation cannot use this. */
+    static constexpr int64_t ALWAYS_ACTIVE = -1;
+};
 
     /**
      * Parameters that influence chain consensus.
